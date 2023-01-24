9987ead6610df2c388f4ce2fd85bc8bb4c153652
Yevheniia Berdnyk <ie.berdnyk@gmail.com>
2023-01-25 01:14:28 +0200

    Added more methods to validate json

--- tests/test_api_requests.py
+++ tests/test_api_requests.py
@@ -20,15 +20,28 @@ def get_block_info():
             params=[hex(int(latest_block_number, 16) - i - 1), True],
             session=session
         ).json()["result"]
-        number = block_info["number"]
+        block_number = block_info["number"]
+        block_hash = block_info["hash"]
         try:
             to_address = block_info["transactions"][0]["to"]
             transaction_hash = block_info["transactions"][0]["hash"]
-            return number, to_address, transaction_hash
+            return block_number, block_hash, to_address, transaction_hash
         except IndexError:
             continue
     else:
-        return "latest", None, None
+        return "latest", None, None, None
+
+
+def get_filter():
+    try:
+        return BaseApiTestCase().post(
+            request_id=11,
+            method="eth_newPendingTransactionFilter",
+            params=[],
+            session=requests.Session()
+        ).json()["result"]
+    except KeyError:
+        return None
 
 
 class TestApiRequest(BaseApiTestCase):
@@ -36,7 +49,8 @@ class TestApiRequest(BaseApiTestCase):
     @classmethod
     def setup_class(cls):
         super().setup_class()
-        cls.block_number, cls.to_address, cls.transaction_hash = get_block_info()
+        cls.block_number, cls.block_hash, cls.to_address, cls.transaction_hash = get_block_info()
+        cls.filter = get_filter()
 
         cls.test_data = {
             "debug_traceBlockByNumber": [
@@ -58,11 +72,90 @@ class TestApiRequest(BaseApiTestCase):
             ],
             "eth_getTransactionReceipt": [
                 cls.transaction_hash
-            ]
+            ],
+            "eth_getTransactionByHash": [
+                cls.transaction_hash
+            ],
+            "trace_block": [cls.block_number],
+            "erigon_blockNumber": ["latest"],
+            "erigon_forks": [],
+            "eth_blockNumber": [],
+            "eth_chainId": [],
+            "eth_protocolVersion": [],
+            "eth_syncing": [],
+            "eth_gasPrice": [],
+            "eth_maxPriorityFeePerGas": [],
+            "eth_feeHistory": ["0x5", "latest", []],
+            "eth_getBlockByHash": [cls.block_hash, True],
+            "eth_getBlockByNumber": [cls.block_number, True],
+            "eth_getBlockTransactionCountByHash": [cls.block_hash],
+            "eth_getBlockTransactionCountByNumber": [cls.block_number],
+            "eth_getTransactionByBlockHashAndIndex": [cls.block_hash, "0x0"],
+            "eth_getTransactionByBlockNumberAndIndex": [cls.block_number, "0x0"],
+            "eth_getBlockReceipts": ["latest"],
+            "eth_call": [
+                {
+                    "from": '0x67b1d87101671b127f5f8714789C7192f7ad340e',
+                    "to": '0xa94f5374Fce5edBC8E2a8697C15331677e6EbF0B'
+                },
+                cls.block_number
+            ],
+            "eth_estimateGas": [
+                {
+                    "from": '0x67b1d87101671b127f5f8714789C7192f7ad340e',
+                    "to": '0xa94f5374Fce5edBC8E2a8697C15331677e6EbF0B'
+                }
+            ],
+            "eth_getBalance": ['0x67b1d87101671b127f5f8714789C7192f7ad340e', 'latest'],
+            "eth_getTransactionCount": ['0x67b1d87101671b127f5f8714789C7192f7ad340e', 'latest'],
+            "eth_getStorageAt": ['0x67b1d87101671b127f5f8714789C7192f7ad340e', '0x0', 'latest'],
+            "eth_newFilter": [{"fromBlock": cls.block_number}],
+            "eth_newBlockFilter": [],
+            "eth_newPendingTransactionFilter": [],
+            "eth_uninstallFilter": [cls.filter],
+            "eth_getLogs": [{"fromBlock": cls.block_number}],
+            "eth_accounts": [],
+            "eth_signTypedData": [],
+            "eth_coinbase": []
+
         }
 
     @pytest.mark.api
-    @pytest.mark.parametrize("method", ("debug_traceBlockByNumber", "trace_filter", "eth_getTransactionReceipt"))
+    @pytest.mark.parametrize(
+        "method", (
+                "debug_traceBlockByNumber",
+                "trace_filter",
+                "eth_getTransactionReceipt",
+                "trace_block",
+                "erigon_blockNumber",
+                "erigon_forks",
+                "eth_getTransactionByHash",
+                "eth_blockNumber",
+                "eth_chainId",
+                "eth_protocolVersion",
+                "eth_syncing",
+                "eth_gasPrice",
+                "eth_maxPriorityFeePerGas",
+                "eth_feeHistory",
+                "eth_getBlockByHash",
+                "eth_getBlockByNumber",
+                "eth_getBlockTransactionCountByHash",
+                "eth_getBlockTransactionCountByNumber",
+                "eth_call",
+                "eth_estimateGas",
+                "eth_getBalance",
+                "eth_getTransactionCount",
+                "eth_getStorageAt",
+                "eth_newFilter",
+                "eth_newBlockFilter",
+                "eth_newPendingTransactionFilter",
+                "eth_uninstallFilter",
+                "eth_getLogs",
+                "eth_accounts",
+                "eth_signTypedData",
+                "eth_coinbase"
+        )
+    )
     def test_api_response_validation(self, method):
         response = self.post(request_id=hex(77), method=method, params=self.test_data[method])
         assert response.status_code == 200, f"Got {response.status_code}, {response}"
